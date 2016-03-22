9441d2555da6430847537073f77efa13be53ca33
郑晓洛 <zhengxiaoluo@haizhi.com>
2016-03-22 11:27:42 +0800

    utf-8 filter test

--- src/main/java/com/lxz/restful/spark/Main.java
+++ src/main/java/com/lxz/restful/spark/Main.java
@@ -5,19 +5,24 @@ import spark.Spark;
 
 @Slf4j
 public class Main {
+    private static final String CHARSET = "UTF-8";
+
     public static void main(String[] args) {
         Spark.get("/hello", (req, res) -> {
-            return "hello world";
+            return "hello world 你好";
         });
 
         Spark.before((req, res) -> {
             log.debug("req before header {}", req.headers());
+            req.raw().setCharacterEncoding(CHARSET);
+
             res.cookie("uid", "x000343", 1000);
             res.header("uuid", "x000111");
         });
 
         Spark.after((req, res) -> {
             log.debug("req after header {}", req.headers());
+            res.raw().setCharacterEncoding(CHARSET);
 
             // res.body(res.body() + "!!!!!");
             res.cookie("uid1", "x000343", 1000);
