0fd1866d8c5b14f294f19bfb61efa1f8ee5b630d
郑晓洛 <zhengxiaoluo@haizhi.com>
2016-03-22 11:09:56 +0800

    logbak test

--- src/main/java/com/lxz/restful/spark/Main.java
+++ src/main/java/com/lxz/restful/spark/Main.java
@@ -1,7 +1,9 @@
 package com.lxz.restful.spark;
 
+import lombok.extern.slf4j.Slf4j;
 import spark.Spark;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) {
         Spark.get("/hello", (req, res) -> {
@@ -9,11 +11,14 @@ public class Main {
         });
 
         Spark.before((req, res) -> {
+            log.debug("req before header {}", req.headers());
             res.cookie("uid", "x000343", 1000);
             res.header("uuid", "x000111");
         });
 
         Spark.after((req, res) -> {
+            log.debug("req after header {}", req.headers());
+
             // res.body(res.body() + "!!!!!");
             res.cookie("uid1", "x000343", 1000);
             res.header("uuid1", "x000111");
